a5918ffce766bb5d43bb773af501edcaffb79e13
Gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-12-11 13:11:36 -0300

    create user functions

---
+++ src/functions/user/index.ts
@@ -0,0 +1,19 @@
+import create from './core/create';
+import read from './core/read';
+import update from './core/update';
+import destroy from './core/destroy';
+import auth from './core/auth';
+import history from './core/history';
+import withdraw from './core/withdraw';
+import indicate from './core/indicate';
+
+export default {
+  create,
+  read,
+  update,
+  destroy,
+  auth,
+  history,
+  withdraw,
+  indicate
+};
