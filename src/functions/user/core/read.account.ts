7c1cad62d0f70634e97c3ae2561ed24af7b9b0d1
Gabriel Cardoso <gabrielhvcardoso@gmail.com>
2020-12-29 20:33:50 -0300

    fix bug

--- src/functions/user/core/read.account.ts
+++ src/functions/user/core/read.account.ts
@@ -3,7 +3,8 @@ import { Account } from '../../../types/account';
 
 const read = async (token: string): Promise<Account | null> => {
   try {
-    const account: Account = (await knex('account').where({ token }))[0];
+    const accounts: Array<Account> = await knex('account').where({ token });
+    const account = accounts[accounts.length - 1];
     return account;
   } catch {
     return null;
