cfcf3a06c862d13ffe49e6fb75d4eedb5a4aff11
Gabriel Cardoso <gabrielhvcardoso@gmail.com>
2021-01-01 16:02:04 -0300

    .

--- src/functions/manager/core/ative.user.ts
+++ src/functions/manager/core/ative.user.ts
@@ -4,7 +4,7 @@ import { Response } from '../../../types/response';
 
 const ative = async (token: string): Promise<Response> => {
   try {
-    await knex('user').update({ new: 0 }).where({ token });
+    await knex('user').update({ new: 0, status: 1 }).where({ token });
     return ({ code: 'success' });
   } catch {
     return ({ code: 'error' });
