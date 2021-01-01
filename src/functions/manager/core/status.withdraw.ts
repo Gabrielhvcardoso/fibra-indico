35056a4e537ce7eb2e70c7f419e90742dc72670a
Gabriel Cardoso <gabrielhvcardoso@gmail.com>
2021-01-01 16:34:07 -0300

    update account on withdraw status

--- src/functions/manager/core/status.withdraw.ts
+++ src/functions/manager/core/status.withdraw.ts
@@ -1,11 +1,22 @@
 import knex from '../../../database';
 
 import { Response } from '../../../types/response';
+import { User } from '../../../types/user';
+import { WithdrawOrder } from '../../../types/withdrawOrder';
 
 const status = async (withdrawOrderId: number, status: string): Promise<Response> => {
   try {
-    await knex('withdrawOrder').where({ withdrawOrderId }).update({ status });
-    return ({ code: 'success' });
+    if (status === 'done') {
+      const withdraw: WithdrawOrder = (await knex('withdrawOrder').where({ withdrawOrderId }))[0];
+      const user: User = (await knex('user').where({ token: withdraw.fromUserToken }))[0];
+      const nextAccount = user.account - withdraw.amount;
+      await knex('withdrawOrder').where({ withdrawOrderId }).update({ status });
+      await knex('user').where({ token: user.token }).update({ account: nextAccount });
+      return ({ code: 'success' });
+    } else {
+      await knex('withdrawOrder').where({ withdrawOrderId }).update({ status });
+      return ({ code: 'success' });
+    }
   } catch (e) {
     console.log(e);
     return ({ code: 'error' });
