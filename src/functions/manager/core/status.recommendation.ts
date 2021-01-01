03cf6c3eb6110ce2914619bb9aebb026e5953ad7
Gabriel Cardoso <gabrielhvcardoso@gmail.com>
2021-01-01 19:54:40 -0300

    adjust recommendation commission

--- src/functions/manager/core/status.recommendation.ts
+++ src/functions/manager/core/status.recommendation.ts
@@ -1,4 +1,5 @@
 import knex from '../../../database';
+import { Hierarchy } from '../../../types/hierarchy';
 import { Product } from '../../../types/product';
 import { Recommendation } from '../../../types/recommendation';
 import { Response } from '../../../types/response';
@@ -7,20 +8,45 @@ import { User } from '../../../types/user';
 const status = async (recommendationId: number, status: string): Promise<Response> => {
   try {
     if (status === 'done') {
+      const hierarchies: Array<Hierarchy> = (await knex('hierarchy')).sort((a, b) => a.depth - b.depth);
       const recommendation: Recommendation = (await knex('recommendation').where({ recommendationId }))[0];
       const product: Product = (await knex('product').where({ productId: recommendation.productId }))[0];
       const user: User = (await knex('user').where({ token: recommendation.fromUserToken }))[0];
 
-      const nextAccount = user.account + product.commission;
+      const commission = product.commission;
 
       await knex('recommendation').where({ recommendationId }).update({ status });
-      await knex('user').where({ token: recommendation.fromUserToken }).update({ account: nextAccount });
+
+      const depth = hierarchies.length;
+
+      let done = [];
+      let todo = [user];
+
+      for (let i = 0; i <= depth; i++) {
+        todo.forEach(async (item) => {
+          const increaseAmount = i === 0
+            ? commission
+            : commission * (hierarchies[i - 1].porcentage / 100);
+
+          console.log(`${i} - ${increaseAmount}`);
+
+          const newAccount = item.account + increaseAmount;
+          await knex('user').where({ token: item.token }).update({ account: newAccount });
+        });
+
+        const children: Array<User> = await knex('user').whereIn('indicatedBy', todo.map(item => item.token));
+
+        done = [...done, ...todo];
+        todo = children;
+      }
+
       return ({ code: 'success' });
     } else {
       await knex('recommendation').where({ recommendationId }).update({ status });
       return ({ code: 'success' });
     }
-  } catch {
+  } catch (e) {
+    console.log(e);
     return ({ code: 'error' });
   }
 };
